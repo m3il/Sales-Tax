697d1a8658bf7a9c18e3bba4d9f012d9b0e59e8b
m3il <96340376+m3il@users.noreply.github.com>
2023-03-09 12:13:43 +0000

    Add files via upload

---
+++ Tax.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Tax{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Retail price of the item: ");
+        double price = in.nextDouble();
+
+        System.out.println("Sales tax rate (%) of the item: ");
+        double tax = in.nextDouble();
+
+        tax = tax / 10;
+        
+        double fullPrice = tax + price;
+
+        System.out.println("The sales tax is: " + tax + " And the full price is: " + fullPrice);
+    }
+}
+    
